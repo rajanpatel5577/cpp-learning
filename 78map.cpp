59c478a3d0f72f7eaeb4da9c83f1164f55dba8b3
rajan.patel5577 <rajan.patel@tidalwave.tech>
2025-06-12 13:25:03 +0530

    changes

--- 78map.cpp
+++ 78map.cpp
@@ -3,13 +3,16 @@
 #include <string>
 
 template <typename T1, typename T2>
-void displayMap(const std::map<T1, T2> &marks) {
-    for (const auto &pair : marks) {
+void displayMap(const std::map<T1, T2> &marks)
+{
+    for (const auto &pair : marks)
+    {
         std::cout << pair.first << ": " << pair.second << std::endl;
     }
 }
 
-int main() {
+int main()
+{
     std::map<std::string, int> marksMap;
 
     marksMap["Alice"] = 85;
@@ -21,12 +24,10 @@ int main() {
 
     displayMap(marksMap);
 
-    std::cout<< "Size of map: " << marksMap.size() << std::endl;
-   std::cout<<"max size of map: " << marksMap.max_size() << std::endl;
-    std::cout<<"Is map empty? " << (marksMap.empty() ? "Yes" : "No") << std::endl;
+    std::cout << "Size of map: " << marksMap.size() << std::endl;
+    std::cout << "max size of map: " << marksMap.max_size() << std::endl;
+    std::cout << "Is map empty? " << (marksMap.empty() ? "Yes" : "No") << std::endl;
 
     // Accessing elements
     std::cout << "Marks of Alice: " << marksMap["Alice"] << std::endl;
-
-    
 }
